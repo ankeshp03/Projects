8b9b8c22e172b260adddfadb2e59a8ae494472da
Paramanik <666108@cognizant.com>
2020-09-14 02:07:17 +0530

    Updated loading component for projects

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,13 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Loader from './DataLoader';
 
 import Home from './Home';
 const D3Charts = lazy(() => import('./D3Charts'));
 
 const Routes = () => {
     return (
-        <Suspense fallback={<span>Loading...</span>}>
+        <Suspense fallback={<Loader />}>
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/projects" component={Home} />
